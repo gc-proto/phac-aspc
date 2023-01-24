59cd30735418f19d5d638533e745f5535e0289af
David Pepin <pepin.david@gmail.com>
2023-01-24 13:20:21 -0500

    Create alpha-banner-fr.js

---
+++ js/alpha-banner-fr.js
@@ -0,0 +1,3 @@
+
+var alphaBanner = document.getElementsByTagName('BODY')[0];
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">Cette page web est en alpha</h2><div><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; Ceci est un manuel de conception expérimetnal - Un travail en progression.</small></div></section >');
