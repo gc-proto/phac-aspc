ffdbf3ffd790e412a927e8225e242f2da79ae7e8
David Pepin <pepin.david@gmail.com>
2023-02-06 10:32:38 -0500

    fixes

--- js/alpha-banner-fr.js
+++ js/alpha-banner-fr.js
@@ -1,3 +1,3 @@
 
 var alphaBanner = document.getElementsByTagName('BODY')[0];
-alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">Cette page web est en alpha</h2><div><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; Ceci est un manuel de conception expérimetnal - Un travail en progression.</small></div></section >');
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top" role="banner"><h2 class="wb-inv">Cette page web est en alpha</h2><div><small><span class="alpha-label">Alpha</span>&nbsp;&nbsp; Ceci est un manuel de conception expérimental - Un travail en progression.</small></div></section >');
